904ff509b0bab15de7b822a12502f343f4df2249
Khaled-Amer <56661090+Khaled-Amer@users.noreply.github.com>
2023-04-05 01:19:10 -0600

    Update TX_FINAL.c

--- TX_FINAL.c
+++ TX_FINAL.c
@@ -69,49 +69,6 @@
 
 #include "ChangeClk.h"
 
-////// CONFIGURATION BITS ////
-//
-//// Code protection 
-////#pragma config BSS = OFF // Boot segment code protect disabled
-////#pragma config BWRP = OFF // Boot sengment flash write protection off
-////#pragma config GCP = OFF // general segment code protecion off
-////#pragma config GWRP = OFF
-//
-//// CLOCK CONTROL 
-//#pragma config IESO = OFF    // 2 Speed Startup disabled
-//#pragma config FNOSC = FRC  // Start up CLK = 8 MHz
-//#pragma config FCKSM = CSECMD // Clock switching is enabled, clock monitor disabled
-//#pragma config SOSCSEL = SOSCLP // Secondary oscillator for Low Power Operation
-//#pragma config POSCFREQ = MS  //Primary Oscillator/External clk freq betwn 100kHz and 8 MHz. Options: LS, MS, HS
-//#pragma config OSCIOFNC = ON  //CLKO output disabled on pin 8, use as IO. 
-//#pragma config POSCMOD = NONE  // Primary oscillator mode is disabled
-//
-//// WDT
-//#pragma config FWDTEN = OFF // WDT is off
-//#pragma config WINDIS = OFF // STANDARD WDT/. Applicable if WDT is on
-//#pragma config FWPSA = PR32 // WDT is selected uses prescaler of 32
-//#pragma config WDTPS = PS1 // WDT postscler is 1 if WDT selected
-//
-//// MCLR/RA5 CONTROL
-////#pragma config MCLRE = OFF // RA5 pin configured as input, MCLR reset on RA5 diabled
-//
-//// BOR  - FPOR Register
-//#pragma config BORV = LPBOR // LPBOR value=2V is BOR enabled
-//#pragma config BOREN = BOR0 // BOR controlled using SBOREN bit
-//#pragma config PWRTEN = OFF // Powerup timer disabled
-//#pragma config I2C1SEL = PRI // Default location for SCL1/SDA1 pin
-
-//// JTAG FICD Register
-//#pragma config BKBUG = OFF // Background Debugger functions disabled
-//#pragma config ICS = PGx2 // PGC2 (pin2) & PGD2 (pin3) are used to connect PICKIT3 debugger
-//
-//// Deep Sleep RTCC WDT
-//#pragma config DSWDTEN = OFF // Deep Sleep WDT is disabled
-//#pragma config DSBOREN = OFF // Deep Sleep BOR is disabled
-//#pragma config RTCOSC = LPRC// RTCC uses LPRC 32kHz for clock
-//#pragma config DSWDTOSC = LPRC // DeepSleep WDT uses Lo Power RC clk
-//#pragma config DSWDTPS = DSWDTPS7 // DSWDT postscaler set to 32768 
-
 // MACROS
 #define Nop() {__asm__ volatile ("nop");}
 #define ClrWdt() {__asm__ volatile ("clrwdt");}
@@ -247,14 +204,8 @@ void send_signal(uint32_t number) {
 int main(void) {
 
     // Analog ports to digital
-  //    AD1PCFGH = 0xFFFF;
-//    ANSELA = 0;
-//    ANSELB = 0;
     ANSA = 0;
     ANSB = 0;
-
-    // Nesting OFF
-    //   INTCON1bits.NSTDIS = 1;
 
     NewClk(8);
 
@@ -263,19 +214,10 @@ int main(void) {
 
     // set A4 as input for the RF transmitter 
     TRISAbits.TRISA4 = 0;
-    
-    // DEBUG LED
-   // TRISAbits.TRISA4 = 0;
-    
- //  TRISAbits.TRISA3 = 0;
-  // LATAbits.LATA3 = 1; // LED ON/Solenoid ON
 
 
     while (1) {
-     //   LATAbits.LATA2 = 1; // LED ON/Solenoid ON
 
-
-//        while (B1 == 0 || B2 == 0 || B3 == 0 || B4 == 0 || B5 == 0) {
             if (B1 == 0) {  //if before
                 send_signal(LEFT);
                 delay_us(5000);
@@ -300,12 +242,8 @@ int main(void) {
             if (B5 == 0) {
                 send_signal(SAFETY);
                 delay_us(5000);
-            }
-
-            
+            }   
         }
-
-   // }
     return 0;
 }
 
